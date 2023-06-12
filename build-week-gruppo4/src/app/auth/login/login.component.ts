577d4dee945d4ea585083e738baf99d63fa157ea
Giafal <gianluca.falcone@yahoo.it>
2023-06-12 17:04:20 +0200

    html ts login

--- build-week-gruppo4/src/app/auth/login/login.component.ts
+++ build-week-gruppo4/src/app/auth/login/login.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { LoginData } from 'src/app/interfaces/login-data';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  data: LoginData = {
+    email: '',
+    password: '',
+  };
 
+  constructor(private authSvc: AuthService, private router: Router) {}
+
+  login() {
+    this.authSvc.login(this.data).subscribe((accessData) => {
+      alert(`Sei loggato come ${accessData.user.name}`);
+      this.router.navigate(['/dashboard']);
+    });
+  }
 }
