1c46581eccb7792ee3478daacb30a8405f22bdf2
hassan_sahraoui@yahoo.itgit config --global user.name hassansahraoui@yahoo.it <hassan_sahraoui@yahoo.it>
2023-06-12 16:29:43 +0200

    modifiche registrazione

--- build-week-gruppo4/src/app/app.module.ts
+++ build-week-gruppo4/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, NgForm } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -16,7 +16,9 @@ import { FooterComponent } from './components/footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
