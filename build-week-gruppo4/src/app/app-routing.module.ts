dce843d1cc5823bbc5d419fda28f66b2106bb1ff
Giafal <gianluca.falcone@yahoo.it>
2023-06-13 10:12:00 +0200

    auth page

--- build-week-gruppo4/src/app/app-routing.module.ts
+++ build-week-gruppo4/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }, { path: 'crud', loadChildren: () => import('./crud/crud.module').then(m => m.CrudModule) }];
+const routes: Routes = [
+  { path: '', redirectTo: 'auth', pathMatch: 'full' },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'crud',
+    loadChildren: () => import('./crud/crud.module').then((m) => m.CrudModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
