8c44c359a0aa5f0910c7d96f3e35a10518b0b05f
Simonecicca <129075136+Simonecicca@users.noreply.github.com>
2023-06-13 17:28:06 +0200

    ok

--- build-week-gruppo4/src/app/app.module.ts
+++ build-week-gruppo4/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { FAQsComponent } from './components/faqs/faqs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    FAQsComponent
   ],
   imports: [
     BrowserModule,
